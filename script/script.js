99998bf88bd985773ac231da8451f77f004636d0
Даниил Щербаков <djdaniil46@gmail.com>
2021-04-11 01:36:12 +0300

    Initial commit

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// =====================================================================
 // our services tabs
 
 const tab = document.querySelector(".service-list-tabs");
@@ -23,11 +23,43 @@ tab.addEventListener("click", function(evt){
 });
 
 // =====================================================================
-
 // our amazing work tabs
 
+// const tabThemesUl = document.querySelector(".topic-switch");
+// const tabThemesLi = document.querySelectorAll(".topic-element");
+// tabThemesUl.addEventListener("click", function(evt){
+//     const dataTab = evt.target.getAttribute("data-tab");
+//     const content = document.querySelectorAll(".js-tab-content");
+//     tabLi.forEach(function(item){
+//         item.classList.remove("active");
+//     })
+//     evt.target.classList.add("active");
+//     content.forEach(article => {
+//         let atr = article.getAttribute("data-tab");
+//         if (atr === dataTab) {
+//             article.classList.add("active");            
+//         }
+//         else{
+//             article.classList.remove("active");
+//         }
+//     });
+// });
 
+
+
+
+// =====================================================================
 // our amazing work onmouseover
+const imageWrapper = document.querySelector(".image-wrapper");
+imageWrapper.addEventListener("mouseover", function(evt){
+    const dataTab = evt.target;
+    
+})
+
+
+
+
+// =====================================================================
 // our amazing work load more
 
 
