9b599a1745536cf5ce588d3495f6c67b27a9ee12
Даниил Щербаков <djdaniil46@gmail.com>
2021-04-10 21:48:03 +0300

    Initial commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,34 @@
 "use strict";
 
-$()
+// our services tabs
+
+const tab = document.querySelector(".service-list-tabs");
+const tabLi = document.querySelectorAll(".js-tab-trigger");
+tab.addEventListener("click", function(evt){
+    const dataTab = evt.target.getAttribute("data-tab");
+    const content = document.querySelectorAll(".js-tab-content");
+    tabLi.forEach(function(item){
+        item.classList.remove("active");
+    })
+    evt.target.classList.add("active");
+    content.forEach(article => {
+        let atr = article.getAttribute("data-tab");
+        if (atr === dataTab) {
+            article.classList.add("active");            
+        }
+        else{
+            article.classList.remove("active");
+        }
+    });
+});
+
+// =====================================================================
+
+// our amazing work tabs
+
+
+// our amazing work onmouseover
+// our amazing work load more
+
+
+// what people say slider
