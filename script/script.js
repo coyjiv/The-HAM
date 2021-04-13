27f71bf4c3f653c48e80e1930cb813772b3f0c78
Даниил Щербаков <djdaniil46@gmail.com>
2021-04-14 02:24:06 +0300

    Initial commit

--- script/script.js
+++ script/script.js
@@ -1,5 +1,13 @@
 "use strict";
 // =====================================================================
+// nav pseudo element hover
+const ulNav =document.querySelector(".header-nav-list");
+ulNav.addEventListener("mouseover", function(evt){
+    const links = evt.target.querySelectorAll(".nav-list-link");
+    console.log(links);
+})
+
+// =====================================================================
 // our services tabs
 
 const tab = document.querySelector(".service-list-tabs");
@@ -50,17 +58,41 @@ tab.addEventListener("click", function(evt){
 
 // =====================================================================
 // our amazing work onmouseover
-const imageWrapper = document.querySelector(".image-wrapper");
-imageWrapper.addEventListener("mouseover", function(evt){
-    const dataTab = evt.target;
+// const imageWrapper = document.querySelector(".image-wrapper");
+// imageWrapper.addEventListener("mouseover", function(evt){
+//     const dataTab = evt.target;
+//     const div = document.querySelector(".creative-design");
+//     div.classList.add("active");
+//     dataTab
     
-})
+// })
 
 
 
 
 // =====================================================================
 // our amazing work load more
+const loadMore = document.querySelector(".load-more");
+loadMore.addEventListener("click", function(evt){
+    evt.preventDefault();
+    const wrapper = document.querySelector(".hidden_wrapper");
+    const section = document.querySelector(".amazing-work");
+    let stepOpacity = 0;
+    let stepHeight = 1212;
+    function animationFade(){
+        wrapper.style.opacity=stepOpacity;
+        stepOpacity+=0.1;
+        section.style.height = stepHeight+"px";
+        stepHeight+=55;
+        if (stepOpacity>=1) {
+            clearInterval(some);
+            return;
+        }
+    }
+    wrapper.style.display="flex";
 
+    let some = setInterval(animationFade, 20);
+    loadMore.style.opacity = 0;
+})
 
 // what people say slider
